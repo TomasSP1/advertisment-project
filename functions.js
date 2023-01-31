748ef2f385a99c0fe51600941196e024fc7d7732
TomasSP <113086584+TomasSP1@users.noreply.github.com>
2023-01-31 15:24:07 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,8 +1,8 @@
-
+const mainContainer = document.querySelector('.main-container');
 
 const creatingRegForm = () => {
-    const registrationContainer = document.querySelector('.registration-form-container');
-    registrationContainer.innerHTML = 
+    const mainLoginFormContainer = document.querySelector('.mainLoginFormContainer');
+    mainLoginFormContainer.innerHTML = 
     `<div class="row justify-content-center">
         <div class="col-md-5">
             <div class="card">
@@ -67,10 +67,134 @@ const creatingRegForm = () => {
 // };
 
 const creatingLogoutBtn = ()=> {
-    const registrationContainer = document.querySelector('.registration-form-container');
-    registrationContainer.innerHTML = `<i class="fa-solid fa-right-from-bracket logoutBtn"></i>`;
+    const logoutBtnContainer = document.querySelector('.logoutBtnContainer');
+    logoutBtnContainer.innerHTML = `<i class="fa-solid fa-right-from-bracket logoutBtn"></i>`;
 };
 
+// functions responsible for display of registered Users in table
+let userNr = 0;
 
 
-export {creatingRegForm, creatingLogoutBtn}
+
+function AddItemToTable(email, role, date, key) {
+    const userTableContainer = document.querySelector('.userTableContainer');
+
+    userTableContainer.innerHTML = `<div class="container table-container my-5">
+                                    <h2>Users:</h2>
+                                    <table class="table table-bordered table-hover user-table">
+                                        <thead>
+                                            <th class="text-center">Nr</th>
+                                            <th class="text-center">email</th>
+                                            <th class="text-center">role</th>
+                                            <th class="text-center">date</th>
+                                            <th class=userDeleteIcon><i class="fa-solid fa-user-xmark"></i></th>
+                                        </thead>
+                                        <tbody class="tbody1"></tbody>
+                                    </table>
+                                </div>`
+    
+    const userMaintable = document.querySelector('.user-table');
+    console.log(userMaintable);
+
+    const tbody1 = document.querySelector('.tbody1');
+    let trow = document.createElement('tr');
+    trow.setAttribute('data-id', key)
+    
+    let td1 = document.createElement('td');
+    td1.classList.add('text-center');
+    
+    let td2 = document.createElement('td');
+    td2.classList.add('text-center');
+    let td3 = document.createElement('td');
+    td3.classList.add('text-center');
+    let td4 = document.createElement('td');
+    td4.classList.add('text-center');
+    let td5 = document.createElement('td');
+    td5.classList.add('deleteUserBtn');
+  
+    td1.innerHTML = ++userNr;
+    td2.innerHTML = email;
+    td3.innerHTML = role;
+    td4.innerHTML = date;
+    td5.innerHTML = `<i class="fa-solid fa-square-minus"></i>`
+  
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
+  
+    tbody1.appendChild(trow);
+    console.log(tbody1)
+    userMaintable.appendChild(tbody1);
+  }
+
+  function AddAllItemsToTable(User, tbody1) {
+    console.log(tbody1)
+    userNr = 0;
+    // tbody1.innerHTML = "";
+  
+      for (let i in User) {
+        AddItemToTable(User[i].email, User[i].role, User[i].timestamp, i);
+      }
+  }
+// end of functions responsible for display of registered Users in table
+
+// functions responsible for display of categories in table
+let catNo = 0;
+const tbody2 = document.querySelector('.tbody2');
+
+function AddCategoryToTable(category, key) {
+  const categoryContainer = document.querySelector('.categoryContainer');
+
+  categoryContainer.innerHTML = `<div class="container table-container my-5">
+                                    <h2>Categories:</h2>
+                                    <table class="table table-bordered table-hover category-table">
+                                        <thead>
+                                            <th class="text-center">Nr</th>
+                                            <th class="categories-input-container"><input type="text" class="form-control category-input" 
+                                            placeholder="Enter category..."><button class="btn btn-primary enterCategoryBtn">Enter</button></th>
+                                            <th class=userDeleteIcon><i class="fa-solid fa-user-xmark"></i></th>
+                                        </thead>
+                                        <tbody class="tbody2"></tbody>
+                                    </table>
+                                </div>`
+
+  const categoryTable = document.querySelector('.category-table')
+
+  let trow = document.createElement('tr');
+  trow.setAttribute('data-id', key);
+
+  let td1 = document.createElement('td');
+  td1.classList.add('text-center');
+
+  let td2 = document.createElement('td');
+  td2.classList.add('text-center');
+
+  let td5 = document.createElement('td');
+  td5.classList.add('delCategoryBtn');
+
+  td1.innerHTML = ++catNo;
+  td2.innerHTML = category;
+  td5.innerHTML = `<i class="fa-solid fa-square-minus"></i>`
+  
+  trow.appendChild(td1);
+  trow.appendChild(td2);
+  trow.appendChild(td5);
+
+  tbody2.appendChild(trow);
+  categoryTable.appendChild(tbody2);
+
+}
+
+function AddAllItemsToCategoryTable(categories) {
+  catNo = 0;
+  tbody2.innerHTML = "";
+
+    for (let i in categories) {
+      AddCategoryToTable(categories[i].category, i);
+    }
+}  
+// end of functions responsible for display of categories in table
+
+export {creatingRegForm, creatingLogoutBtn, AddAllItemsToTable, AddAllItemsToCategoryTable}
--- script.js
+++ script.js
@@ -18,8 +18,9 @@ import { getAuth,
 
 import {firebaseConfig} from "./firebase.js"
 import {creatingRegForm,
-        creatingLogoutBtn
-        
+        creatingLogoutBtn,
+        AddAllItemsToTable,
+        AddAllItemsToCategoryTable
         } from "./functions.js";
 
 
@@ -35,14 +36,13 @@ const database = getDatabase(app);
 
 
 const auth = getAuth();
-
+const mainLoginFormContainer = document.querySelector('.mainLoginFormContainer');
 
 
 // userio statuso patikrinimas
 
 const user = auth.currentUser;
 
-const registrationContainer = document.querySelector('.registration-form-container');
 
 onAuthStateChanged(auth, (user) => {
     
@@ -51,18 +51,107 @@ onAuthStateChanged(auth, (user) => {
         console.log(user)
         console.log(user.uid)
         console.log('useris prisijunges')
-
+        mainLoginFormContainer.innerHTML = '';
         creatingLogoutBtn();
+        console.log(creatingLogoutBtn);
 
         const logOutBtn = document.querySelector(".logoutBtn");
         logOutBtn.addEventListener('click', logOutBtnFunction);
 
+        // sita dalis atspausdina lentele su Userio duomenimis, taip pat leidzia trinti tiek is firebase tiek is lenteles
+        get(ref(database, 'Users/')).then((snapshot) => {
+          
+          const userData = snapshot.val();
+
+          AddAllItemsToTable(userData);
+
+
+          // selecting information delete button
+          const deleteUserBtns = document.querySelectorAll('.deleteUserBtn');
+          deleteUserBtns.forEach(btn => {
+            btn.addEventListener('click', ()=> {
+              const uniqueBtnID = btn.parentElement.getAttribute('data-id');
+              get(ref(database, `Users/${uniqueBtnID}`)).then((snapshot) => {
+                console.log(uniqueBtnID)
+                if (snapshot.exists()) {
+                  remove(ref(database, `Users/${uniqueBtnID}`))
+                  .then(()=> {
+                    alert("Data deleted successfully")
+                    window.location.reload();
+                  })
+                  .catch((error)=> {
+                    alert(error);
+                  });
+                  } else {
+                    console.log("No data available")
+                  }
+                })
+              })
+            })
+      });
+
+      const categoryBtn = document.querySelector('.enterCategoryBtn');
+      const categoryInput = document.querySelector('.category-input');
+      
+
+      // saving category data to firebase
+
+      categoryBtn.addEventListener('click', ()=> {
+        console.log('paspaudziau')
+        set(ref(database, 'categories/' + categoryInput.value), {
+          category: categoryInput.value
+        })
+        .then(()=> {
+          alert("Data added successfully")
+        })
+        .catch((error)=> {
+          alert(error)
+        })
+      });
+
+
+
+      // making functions to build catgeory tables from firebase
+      get(ref(database, 'categories/')).then((snapshot) => {
+          
+        
+        const userData = snapshot.val();
+        console.log(userData)
+
+        AddAllItemsToCategoryTable(userData);
+        
+        // deleting category from firebase
+        const delCategoryBtns = document.querySelectorAll('.delCategoryBtn');
+        delCategoryBtns.forEach(btn => {
+          btn.addEventListener('click', ()=> {
+            const uniqueBtnID = btn.parentElement.getAttribute('data-id');
+            get(ref(database, `categories/${uniqueBtnID}`)).then((snapshot) => {
+              if (snapshot.exists()) {
+                  remove(ref(database, `categories/${uniqueBtnID}`))
+                  .then(()=> {
+                    alert("Data deleted successfully")
+                    window.location.reload();
+                  })
+                  .catch((error)=> {
+                    alert(error);
+                  });
+                  } else {
+                    console.log("No data available")
+                  }
+                })
+              
+              })
+            })
+      });
+
 
     } else {
+        
         
         creatingRegForm();
 
         const loginBtn = document.querySelector('.login-btn');
+        console.log(loginBtn)
         const registerBtn = document.querySelector('.register-btn');
 
         registerBtn.addEventListener('click', registerUserFunction);
@@ -78,6 +167,7 @@ onAuthStateChanged(auth, (user) => {
 
 const logOutBtnFunction = (e) => {
     e.preventDefault();
+    console.log('mygtukas vekia')
     signOut(auth)
       .then(() => {
         // Sign-out successful.
@@ -122,6 +212,7 @@ const loginBtnFunction = (e) => {
         e.preventDefault();
         const user_email = document.getElementById("email").value;
         const user_pass = document.getElementById("password").value;
+        console.log('spaudziu mygtuka')
         signInWithEmailAndPassword(auth, user_email, user_pass)
           .then((userCredential) => {
             // ...
@@ -134,183 +225,44 @@ const loginBtnFunction = (e) => {
 };
 
 
-const dbRef = ref(getDatabase());
-
-let stdNo = 0;
-const tbody1 = document.querySelector('.tbody1');
-console.log(tbody1)
-
-function AddItemToTable(email, role, date, key) {
-  
-  let trow = document.createElement('tr');
-  trow.setAttribute('data-id', key)
-  
-  let td1 = document.createElement('td');
-  td1.classList.add('text-center');
-  
-  let td2 = document.createElement('td');
-  td2.classList.add('text-center');
-  let td3 = document.createElement('td');
-  td3.classList.add('text-center');
-  let td4 = document.createElement('td');
-  td4.classList.add('text-center');
-  let td5 = document.createElement('td');
-  td5.classList.add('deleteUserBtn');
-
-  td1.innerHTML = ++stdNo;
-  td2.innerHTML = email;
-  td3.innerHTML = role;
-  td4.innerHTML = date;
-  td5.innerHTML = `<i class="fa-solid fa-square-minus"></i>`
-
-  trow.appendChild(td1);
-  trow.appendChild(td2);
-  trow.appendChild(td3);
-  trow.appendChild(td4);
-  trow.appendChild(td5);
-
-  tbody1.appendChild(trow);
-}
-
-function AddAllItemsToTable(User) {
-  stdNo = 0;
-  tbody1.innerHTML = "";
-
-    for (let i in User) {
-      AddItemToTable(User[i].email, User[i].role, User[i].timestamp, i);
-    }
-}
 
 
 
-
-
-// sita dalis atspausdina lentele su Userio duomenimis, taip pat leidzia trinti tiek is firebase tiek is lenteles
-  get(child(dbRef, 'Users/')).then((snapshot) => {
-    
-  
-      const userData = snapshot.val();
-  
-      AddAllItemsToTable(userData);
-
-
-      // selecting information delete button
-      const deleteUserBtns = document.querySelectorAll('.deleteUserBtn');
-      deleteUserBtns.forEach(btn => {
-        btn.addEventListener('click', ()=> {
-          const uniqueBtnID = btn.parentElement.getAttribute('data-id');
-          get(child(dbRef, `Users/${uniqueBtnID}`)).then((snapshot) => {
-            console.log(uniqueBtnID)
-            if (snapshot.exists()) {
-              remove(ref(database, `Users/${uniqueBtnID}`))
-              .then(()=> {
-                alert("Data deleted successfully")
-                window.location.reload();
-              })
-              .catch((error)=> {
-                alert(error);
-              });
-              } else {
-                console.log("No data available")
-              }
-            })
-          
-          })
-        })
-  });
   
 
 
 
-        
- const categoryBtn = document.querySelector('.enterCategoryBtn');
- const categoryInput = document.querySelector('.category-input');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
- 
-// saving category data to firebase
-
-categoryBtn.addEventListener('click', ()=> {
-  console.log('paspaudziau')
-  set(ref(database, 'categories/' + categoryInput.value), {
-    category: categoryInput.value
-  })
-  .then(()=> {
-    alert("Data added successfully")
-  })
-  .catch((error)=> {
-    alert(error)
-  })
-});
-
-
-// making functions to build catgeory tables from firebase
-let catNo = 0;
-const tbody2 = document.querySelector('.tbody2');
-
-function AddCategoryToTable(category, key) {
-  let trow = document.createElement('tr');
-  trow.setAttribute('data-id', key);
-
-  let td1 = document.createElement('td');
-  td1.classList.add('text-center');
-
-  let td2 = document.createElement('td');
-  td2.classList.add('text-center');
-
-  let td5 = document.createElement('td');
-  td5.classList.add('delCategoryBtn');
-
-  td1.innerHTML = ++catNo;
-  td2.innerHTML = category;
-  td5.innerHTML = `<i class="fa-solid fa-square-minus"></i>`
-  
-  trow.appendChild(td1);
-  trow.appendChild(td2);
-  trow.appendChild(td5);
-
-  tbody2.appendChild(trow);
-
-}
-
-function AddAllItemsToCategoryTable(categories) {
-  catNo = 0;
-  tbody2.innerHTML = "";
-
-    for (let i in categories) {
-      AddCategoryToTable(categories[i].category, i);
-    }
-}  
-
-
-get(child(dbRef, 'categories/')).then((snapshot) => {
-    
-  
-  const userData = snapshot.val();
-  console.log(userData)
-
-  AddAllItemsToCategoryTable(userData);
-  
-  // deleting category from firebase
-  const delCategoryBtns = document.querySelectorAll('.delCategoryBtn');
-  delCategoryBtns.forEach(btn => {
-    btn.addEventListener('click', ()=> {
-      const uniqueBtnID = btn.parentElement.getAttribute('data-id');
-      get(child(dbRef, `categories/${uniqueBtnID}`)).then((snapshot) => {
-        if (snapshot.exists()) {
-            remove(ref(database, `categories/${uniqueBtnID}`))
-            .then(()=> {
-              alert("Data deleted successfully")
-              window.location.reload();
-            })
-            .catch((error)=> {
-              alert(error);
-            });
-            } else {
-              console.log("No data available")
-            }
-          })
-        
-        })
-      })
-});
 
